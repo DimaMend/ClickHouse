cb61057152130cb6f074fe49304e0957fdd0f057
Aleksandr Musorin <aleksandr.musorin@semrush.com>
2025-06-02 11:56:13 +0200

    tests: add table and database for 02124_insert_deduplication_token_replica
    Fixes:
    
    ```
    Code: 158. DB::Exception: Limit for rows (controlled by 'max_rows_to_read' setting) exceeded, max rows: 20.00 million, current rows: 134.60 million. (TOO_MANY_ROWS) (version 25.4.1.36938 (official build)) (from [::1]:60320) (comment: 02124_insert_deduplication_token_replica.sql)
    ```
    
    Co-authored-by: Azat Khuzhin <a3at.mail@gmail.com>

--- tests/queries/0_stateless/02124_insert_deduplication_token_replica.sql
+++ tests/queries/0_stateless/02124_insert_deduplication_token_replica.sql
@@ -16,6 +16,7 @@ SELECT * FROM insert_dedup_token1 ORDER BY id;
 SYSTEM FLUSH LOGS system.part_log;
 SELECT DISTINCT exception FROM system.part_log
 WHERE table = 'insert_dedup_token1'
+  AND database = currentDatabase()
   AND event_type = 'NewPart'
   AND error = 389;
 
